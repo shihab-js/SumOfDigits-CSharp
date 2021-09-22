c8bf85ef453d6240297198d7a3f17511a1663c09
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 12:05:55 +0600

    Add project files.

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
