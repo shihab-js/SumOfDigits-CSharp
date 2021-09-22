ee8d761bfc51b0c080360ab3fc4be9a41a1a4755
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 12:15:01 +0600

    Programme has been completed!

--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -6,7 +6,27 @@ namespace SumOfDigits
     {
         static void Main(string[] args)
         {
-            
+            //declare necessary variables
+            int number, sum = 0, remainder, temp;
+
+            //print message to show the user
+            Console.Write("Please Enter a Number: ");
+
+            //get input from user
+            number = int.Parse(Console.ReadLine());
+
+            //store it in a temp variable
+            temp = number;
+
+            //continue the program until number greater than 0
+            while (number > 0)
+            {
+                remainder = number % 10;
+                sum = sum + remainder;
+                number = number / 10;
+
+            }
+            Console.Write("Sum of " + temp + " is " + sum);
         }
     }
 }
